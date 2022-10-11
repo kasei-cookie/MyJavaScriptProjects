acaaaf46987b65518adc04f6b8680c4458d38345
kasei-cookie <kasei.sama@gmail.com>
2022-10-11 23:40:22 +0200

    Add Guess a color, change buttons, rearrange CSS

---
+++ guessAColor.js
@@ -0,0 +1,54 @@
+`use strict`;
+
+const colorsArray = [{}, {}, {}, {}, {}, {}];
+let hasEventListeners;
+
+function theMain() {
+    document.querySelector(".new-color").innerText = `New colors!`;
+
+    // Choosing random rgb for
+    function color() {
+        return Math.trunc(Math.random() * 256);
+    }
+
+    // Assigning r g b to every object in an array + Setting background color to each block
+    const colorBlocks = document.querySelectorAll(".color-block");
+
+    for (let i = 0; i < colorsArray.length; i++) {
+        colorsArray[i].r = color();
+        colorsArray[i].g = color();
+        colorsArray[i].b = color();
+        colorBlocks[i].style.backgroundColor = `rgb(${colorsArray[i].r}, ${colorsArray[i].g}, ${colorsArray[i].b})`;
+        colorBlocks[i].innerText = `?`;
+
+        const sum = colorsArray[i].r + colorsArray[i].g + colorsArray[i].b;
+        if (sum < 382) {
+            colorBlocks[i].style.color = "lightgrey";
+        } else {
+            colorBlocks[i].style.color = "black";
+        }
+    }
+
+    // Showing rgb in title
+    let rgbNumber = Math.trunc(Math.random() * 6);
+    document.querySelector(".rgb-title").innerHTML = `RGB (${colorsArray[rgbNumber].r}, ${colorsArray[rgbNumber].g}, ${colorsArray[rgbNumber].b})`
+
+
+    // Checking which color block was clicked
+    if (!hasEventListeners) {
+        for (let i = 0; i < colorsArray.length; i++) {
+            colorBlocks[i].addEventListener("click", function () {
+                if (i === rgbNumber) {
+                    colorBlocks[i].innerHTML = `Correct!`;
+                } else {
+                    colorBlocks[i].innerHTML = `Try again!`;
+                }
+            })
+        }
+        hasEventListeners = true;
+    }
+
+
+}
+
+document.querySelector(".new-color").addEventListener("click", theMain);
